3c8916f7c6a2d3bd41e0ddb561f940eb36967cc2
okamotoR <takehiroshitakehiii@icloud.com>
2021-03-14 02:03:58 +0900

    jpeg rayon

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 extern crate wasm_bindgen;
 extern crate image;
-extern crate rayon;
 extern crate imageproc;
 extern crate console_error_panic_hook;
 
@@ -63,7 +62,7 @@ pub fn raw_img_to_gray_vec(raw_data: Vec<u8>, extension: String) -> Vec<u8> {
 
 pub fn decode_raw_data(raw_data: Vec<u8>, extension: String) -> DynamicImage {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
-    let mut reader = Reader::new(Cursor::new(raw_data));
+    let mut reader = Reader::new(Cursor::new(&raw_data));
     match extension.as_str() {
         "jpg" => reader.set_format(image::ImageFormat::Jpeg),
         "JPG" => reader.set_format(image::ImageFormat::Jpeg),
